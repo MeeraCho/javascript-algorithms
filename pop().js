f857e8c91db449f3f51efaeb6d33a02e3ae13f17
MiraCho <miracho119@gmail.com>
2023-09-07 23:43:23 -0600

    pop(), push()

---
+++ pop().js
@@ -0,0 +1,5 @@
+const threeArr = [1, 4, 6];
+const oneDown = threeArr.pop();
+
+console.log(oneDown); //6
+console.log(threeArr); // 1, 4
---
+++ push().js
@@ -0,0 +1,6 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+myArray.push(["dog", 3]); 
+
+//const myArray = [["John", 23], ["cat", 2], ["dog", 3]];
