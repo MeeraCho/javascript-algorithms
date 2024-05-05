27d1347a44450886ab80c190dbde20a50acfbb8d
MiraCho <miracho119@gmail.com>
2024-05-04 23:02:07 -0600

    added reverse-string testing

---
+++ reverse-string/reverse-string.js
@@ -0,0 +1,6 @@
+// Solution 1
+function reverseString(str){
+    return str.split('').reverse().join('');
+};
+
+module.exports = reverseString;
---
+++ reverse-string/reverse-string.test.js
@@ -0,0 +1,18 @@
+const reverseString = import('./reverse-string');
+
+describe('reverse-string', () => {
+    it('should be a function', () => {
+        expect(typeof reverseString).toEqual('function'); //function itself
+    });
+
+    it('should return a string', () => {
+        expect(typeof reverseString('hello')).toEqual('string'); //after invoking the function
+    });
+
+    it('should return the reversedstring', () => {
+        expect(reverseString('hello')).toEqual('olleh'); 
+        expect(reverseString('bye')).toEqual('eyb'); 
+        expect(reverseString('wow')).toEqual('wow'); //palindrome 팔른즈롬
+        expect(reverseString('hello world')).toEqual('dlrow olleh'); 
+    });
+});
