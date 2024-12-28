2bfe9deb6852887f9f9b3d1157d70c036817605c
Mira Cho <123275133+MeeraCho@users.noreply.github.com>
2024-12-27 23:34:02 -0700

    Update reverse-string-test.js

--- 2.basic-challenges/07-reverse-string/reverse-string-test.js
+++ 2.basic-challenges/07-reverse-string/reverse-string-test.js
@@ -1,7 +1,9 @@
 const reverseString = require('./reverse-string');
 
 test('Reversing a string', () => {
+  
   expect(reverseString('Hello')).toBe('olleH');
   expect(reverseString('JavaScript')).toBe('tpircSavaJ');
   expect(reverseString('12345')).toBe('54321');
+  
 });
