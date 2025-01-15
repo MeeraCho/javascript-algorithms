9548674d1b0902dda4229fa0dc9d84ed0ab7db07
MiraCho <miracho119@gmail.com>
2025-01-14 23:38:54 -0700

    updated remove-duplicates question

--- 1.basic/10.remove-duplicates.js
+++ 1.basic/10.remove-duplicates.js
@@ -1,17 +1,25 @@
 // Q: Write a function called removeDuplicates that takes in an array and returns a new array with duplicates removed.
 
+//---- solution 1 ----
 function removeDuplicates(arr) {
     const uniqueArr = [];
 
-    for (let i = 0; i < arr.length; i++) {
-        if (!uniqueArr.includes(arr[i])) {
+    for(let i = 0; i < arr.length; i++){
+        arr[i] =arr[i].toLowerCase();
+    }
+
+    //const lowerCasedArr = arr.map(item => item.toLowerCase());
+
+    for(let i = 0; i < arr.length; i++){
+        if(!uniqueArr.includes(arr[i])){
             uniqueArr.push(arr[i]);
         }
     }
-
     return uniqueArr;
 }
 
+
+//---- solution 2 ----
 function removeDuplicates2(arr) {
     return Array.from(new Set(arr));
 }
